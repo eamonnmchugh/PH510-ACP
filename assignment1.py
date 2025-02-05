f29f93fb2bea747a8964c05e78bd07ebe3865d3a
eamonnmchugh <eamonn.mchugh.2020@uni.strath.ac.uk>
2025-02-05 22:46:00 +0000

    Add files via upload

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,10 @@
 This program utilises a definite integral (mid-point rule quadrature) to calculate the value of pi
 in parallel
 """
-
+import time
 from mpi4py import MPI
+from mpmath import quad
+
 
 comm = MPI.COMM_WORLD
 
@@ -15,7 +17,7 @@ nproc = comm.Get_size()
 nworkers = nproc - 1
 
 # samples
-N = 1000000
+N = 16
 DELTA = 1.0 / N
 
 # integral
@@ -26,6 +28,7 @@ def integrand(midpoint):
     The function of x is 4/(1 + x^2)"""
     return 4.0 / (1.0 + midpoint**2)
 
+start_time = time.time()
 
 if comm.Get_rank() == 0:
 
@@ -42,7 +45,7 @@ if comm.Get_rank() == 0:
 
         if j == 0:
       # so do this locally using the leader machine
-            y = integrand(x) * DELTA #mpmath
+            y = quad(integrand, [i * DELTA, (i+1) * DELTA])
         else:
       # communicate to a worker
             comm.send(x, dest=j)
@@ -50,12 +53,12 @@ if comm.Get_rank() == 0:
 
         I += y
 
+    print("The code was run using", nproc, "processors")
+    print("From the integral, the value of pi was found to be", f"{I}")
+
   # Shut down the workers
     for i in range(1, nproc):
         comm.send(-1.0, dest=i)
-
-    print("The code was run using", nproc, "processors")
-    print("From the integral, the value of pi was found to be", f"{I}")
 
 else:
 
@@ -70,4 +73,7 @@ else:
       # stop the worker
             break
 
-        comm.send(integrand(x) * DELTA, dest=0)
+        comm.send(quad(integrand, [i * DELTA, (i+1) * DELTA]), dest=0)
+
+end_time = time.time()
+print("The runtime was", f"{end_time - start_time}", "seconds")
