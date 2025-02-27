4941c5374683d3d088d128a936a91286f6b8ed87
eamonnmchugh <eamonn.mchugh.2020@uni.strath.ac.uk>
2025-02-27 10:03:34 +0000

    Improvement on previous code for multiple points
    Updated the point generating class to work for multiple points. The random points were generated in an n x d array where 'n' is the number of points and 'd' the dimensions. The radial calculation was also reworked to produce an n x 1 array. After creating the function 'within', the number of points found within or on the d-dimensional ball was recorded (the percentage of which was also taken).

--- assignment3.py
+++ assignment3.py
@@ -11,39 +11,79 @@ import numpy as np
 
 # ------------------------------------------------------------------------------------------------
 
-class Point:
+class Points:
     """
-    Chooses a random point within a box of 'd' dimensions with values between the range of -1 and 1
+    Generates 'n' random points within a box of 'd' dimensions with constituent coord values
+    between the range of -1 and 1
     """
-    def __init__(self, d):
+    def __init__(self, d, n):
+        self.n = n
         self.d = d
-        self.point = np.random.uniform(-1, 1, d)
+        j = 0
+        points = np.zeros((self.n, self.d))
+        while j < self.n:
+            i = 0
+            A = np.random.uniform(-1, 1, self.d)
+            A_reshape = A.reshape((1, self.d))
+            while i < self.d:
+                points[j][i] = A_reshape[0][i]
+                i = i + 1
+            j = j + 1
+        self.points = points
 
     def __str__(self):
         """
-        Returns the point 
+        Returns the points
         """
-        return f"Point:({self.point})"
+        return f"Points:({self.points})"
 
     def r_vector(self):
         """
-        Calculates the magnitude of the d-dimensional vector by finding the Root Sum Square (RSS)
-        of its constituent components
+        Calculates the magnitude of the d-dimensional vectors by finding the Root Sum Square (RSS)
+        of the points' coords
         """
-        i = 0
-        dim_sum = 0
-        while i < self.d:
-            dim_sum = dim_sum + (self.point[i])**2
-            i = i + 1
-        r = np.sqrt(dim_sum)
+        j = 0
+        dim_sum = np.zeros((self.n, 1))
+        r = np.zeros((self.n, 1))
+        while j < self.n:
+            i = 0
+            while i < self.d:
+                dim_sum[j][0] = dim_sum[j][0] + (self.points[j][i])**2
+                r[j][0] = np.sqrt(dim_sum[j][0])
+                i = i + 1
+            j = j + 1
         return r
 
-A = Point(2)
-print("Example: 2-dimensional point", A)
-A_sum = A.r_vector()
-print(A_sum)
+    def box_location(self):
+        """
+        
+        """
+        within = 0
+        j = 0
+        while j < self.n:
+            if self.r_vector()[j][0] <= 1:
+                within = within + 1
+                j = j + 1
+            else:
+                j = j + 1
+        return within, within/self.n * 100
+        
 
-B = Point(3)
-print(B)
+A = Points(2, 1)
+print("Example: 2D", A)
+A_sum = A.r_vector()
+print("With magnitude", A_sum)
+A_within = A.box_location()
+print(A_within)
+
+B = Points(3, 1)
+print("Example: 3D", B)
 B_sum = B.r_vector()
-print(B_sum)
+print("With magnitude", B_sum)
+
+C = Points(2, 1000)
+print("Example: 5 2D",C)
+C_sum = C.r_vector()
+print("With magnitudes", C_sum)
+C_within = C.box_location()
+print(C_within)
